116babad7ef65ebf0648cd7a3146519c86b92b85
Aishu-ai <84241797+Aishu-ai@users.noreply.github.com>
2021-06-10 22:17:18 +0530

    Update Permutation.java

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -9,19 +9,19 @@ public class Permutation{
 		int n=Integer.parseInt(args[0]);
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		String s=input.readLine();
-		//Path filepath=Paths.get(s);
-		RandomizedQueue<String> r=new RandomizedQueue<String>();
-		//tring content = Files.readString(filepath);
-		String use[]=s.split(" ");
+		
+		RandomizedQueue<String> r=new RandomizedQueue<//tring content = Files.readString(filepath);
+		String use[]=s.split(" ");//splitting the string at blank spaces
 		for(String st:use)
 		r.enqueue(st);
 		int j=r.size();
+		//exiting if k is greater than size of queue	
 		if(n>j){
 			System.out.println("Only "+j+" elements are present.");
 			System.exit(1);
 		}	
 		for(int i=0;i<n;i++)
-		System.out.println(r.dequeue());
+		System.out.println(r.dequeue());//printing k random elements
 	}
 	catch (IOException ioex) {
         ioex.printStackTrace();
